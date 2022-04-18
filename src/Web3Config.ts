e98af390345bcbe4d26b8f6204021fea0e48b806
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-04-18 15:10:24 -0500

    support for sdk v3

--- src/Web3Config.ts
+++ src/Web3Config.ts
@@ -2,7 +2,7 @@ import store from './state/store';
 
 export default class Web3Config {
     static CHAIN = 4;
-    static DOTNUGG_V1 = '0x5dc1a0e59729c0c29b2b659e452a34e2b4f987f1';
+    static DOTNUGG_V1 = '0x328489d531da7e632073b5850a6932e57e8f2698';
     static INFURA_KEY = 'a1625b39cf0047febd415f9b37d8c931';
 
     static getUrls(key?: string) {
