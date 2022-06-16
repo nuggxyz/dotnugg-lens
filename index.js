402f3ac245cda2388c9f6efb44e5fbc995512eca
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-06-16 18:20:34 -0500

    freaking electron

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+// if (__DEV__) {
+
+// } else {
+//     require('./build/server/index.js');
+// }
+// // require('./server/index.ts');
+
 // eslint-ignore
-require('ts-node').register({
-    project: './server/tsconfig.json',
-    files: ['./server'],
-    options: {
-        experimentalReplAwait: true,
-        experimentalResolver: true,
-    },
-}); // This will register the TypeScript compiler
+// require('ts-node').register({
+//     project: './server/tsconfig.json',
+//     files: ['./server'],
+//     options: {
+//         experimentalReplAwait: true,
+//         experimentalResolver: true,
+//         transpileOnly: true,
+//         ignoreDiagnostics: [2339],
+//     },
+// }); // This will register the TypeScript compiler
+// require('./server/index.ts');
 
 globalThis.__DEV__ = process.env.NODE_ENV === 'development';
 
-require('./server/index.ts');
+console.log('AYOOOOOOOO', __DEV__);
+
+require('./serverjs/index.js');
---
+++ serverjs/utils.js
@@ -0,0 +1,15 @@
+const os = require('os');
+
+const pathDelimiter = () => {
+    return os.platform() === 'win32' ? '\\' : '/';
+};
+
+const asepritePath = () => {
+    let asepriteLocation = '"/Applications/Aseprite.app/Contents/MacOS/aseprite"';
+    if (os.platform() === 'win32') {
+        asepriteLocation = '"C:\\Program Files\\Aseprite\\Aseprite.exe"';
+    }
+    return asepriteLocation;
+};
+
+module.exports = { pathDelimiter, asepritePath };
