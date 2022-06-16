e18e49d08a84240a84fed9826dfb339fdc1ea90e
dub6ix <90335716+dub6ix@users.noreply.github.com>
2022-06-15 19:46:58 -0500

    workin'

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 require('ts-node').register({
     project: './server/tsconfig.json',
     files: ['./server'],
+    options: {
+        experimentalReplAwait: true,
+        experimentalResolver: true,
+    },
 }); // This will register the TypeScript compiler
 
 globalThis.__DEV__ = process.env.NODE_ENV === 'development';
